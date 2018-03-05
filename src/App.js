2cae168fdf1b2166b182e5f012736030c38e17d5
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-03-05 22:31:50 +0700

    :tada: Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <div id="container">
+            Make
+          <div id="flip">
+           <div>
+             <div>Coding</div>
+           </div>
+           <div>
+             <div>Yourself</div>
+           </div>
+           <div>
+             <div>Everything</div>
+           </div>
+          </div>
+            AweSoMe!
+         </div>
+        <p className="App-intro">
+          Made with <span class="love">♥</span> in Purwasari
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
